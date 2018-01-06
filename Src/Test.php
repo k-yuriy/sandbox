ca1a889045a6091e0bf43ecdf73899bd0854ae40
yuriy <glwinian@gmail.com>
2018-01-07 01:54:43 +0500

    PSR-4 test structure

---
+++ Src/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuriy
+ * Date: 07.01.2018
+ * Time: 1:46
+ */
+
+namespace Src;
+
+
+class Test
+{
+    public function run()
+    {
+        echo 'hello world!';
+    }
+}
---
+++ cli.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuriy
+ * Date: 07.01.2018
+ * Time: 1:47
+ */
+require(__DIR__ . '/vendor/autoload.php');
+
+(new Src\Test())->run();
